6ab7df7e90a355d017228101161f718cd347ae12
Abdel <112128079+DjeAbdel0@users.noreply.github.com>
2024-10-26 13:10:25 -0400

    page-service terminée + responsive

--- wp-content/themes/page_statique/main.js
+++ wp-content/themes/page_statique/main.js
@@ -1,7 +1,7 @@
-/*   Abdel
-  ---------------------- Code qui lorsqu'on Hover une nouvelle, rend le texte visible et lorqu'on quitte, le cache ------------------------------ */
-// Select all elements that have class 'list-service__assos', 'list-service__truc', 'list-service__cool', etc.
-const serviceDiv = document.querySelectorAll(".list-service > div"); // Choisis la div enfant directe de .list-service
+/* -------------   Abdel    ------------- */
+/* ---------------------- Code qui lorsqu'on Hover une nouvelle, rend le texte visible et lorqu'on quitte, le cache ------------------------------ */
+
+const serviceDiv = document.querySelectorAll(".page-service > div"); // Choisis la div enfant directe de .list-service
 const serviceTexte = document.querySelectorAll(".page-service__texte"); // Choisis le texte dans .list-service
 const serviceBtn = document.querySelectorAll(".page-service__btn"); //  Choisis le btn dans .list-service
 
@@ -21,41 +21,8 @@ serviceDiv.forEach((element, index) => {
     serviceBtn[index].style.visibility = "hidden";
   });
 });
-/*
-const serviceImages = document.querySelectorAll('.page-service__assos img, .page-service__crr img, .page-service__locataire img');
-
-function updateImageSources() {
-  const screenWidth = window.innerWidth;
-
-  serviceImages.forEach((img) => {
-    if (screenWidth >= 765) {
-      if (img.parentElement.classList.contains('page-service__assos')) {
-        img.src = '../assets/medias/services/memoire.jpg'; // Change to desired image
-      } else if (img.parentElement.classList.contains('page-service__crr')) {
-        img.src = '../assets/medias/services/memoire.jpg'; // Change to desired image
-      } else if (img.parentElement.classList.contains('page-service__locataire')) {
-        img.src = '../assets/medias/services/memoire.jpg'; // Change to desired image
-      }
-    } else {
-      // Reset to original images
-      if (img.parentElement.classList.contains('page-service__assos')) {
-        img.src = '../assets/medias/services/comite-association.jpg';
-      } else if (img.parentElement.classList.contains('page-service__crr')) {
-        img.src = '../assets/medias/services/meeting-crr.jpg';
-      } else if (img.parentElement.classList.contains('page-service__locataire')) {
-        img.src = '../assets/medias/services/Donner_cle-locataire.jpg';
-      }
-    }
-  });
-}
 
 
-// Initial check
-updateImageSources();
-
-// Event listener for window resize
-window.addEventListener('resize', updateImageSources);
-  */
 
 document.addEventListener("DOMContentLoaded", function () {
   const items = document.querySelectorAll(".hero__item");
