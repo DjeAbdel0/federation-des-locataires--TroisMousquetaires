274021a8c98a2c23826c8fdf9c1607a5392ec264
Abdel <djeabdel0@gmail.com>
2024-11-06 22:45:16 -0500

    remise finale - Gsap enlever service

--- wp-content/themes/page_statique/main.js
+++ wp-content/themes/page_statique/main.js
@@ -5,6 +5,8 @@
 const serviceDiv = document.querySelectorAll(".page-service > div"); // Choisis la div enfant directe de .list-service
 const serviceTexte = document.querySelectorAll(".page-service__texte"); // Choisis le texte dans .list-service
 const serviceBtn = document.querySelectorAll(".page-service__btn"); //  Choisis le btn dans .list-service
+const serviceDivCrr = document.querySelectorAll(".page-service-crr > div"); // Choisis la div enfant directe de .list-service
+const serviceTexteCrr = document.querySelectorAll(".page-service-crr__texte-crr"); // Choisis le texte dans .list-service
 
 // Toogle la visibilité selon si la souris Hover ou pas
 if (serviceDiv !== null) {
@@ -14,6 +16,8 @@ if (serviceDiv !== null) {
       serviceTexte[index].style.visibility = "visible";
       serviceBtn[index].style.opacity = "1";
       serviceBtn[index].style.visibility = "visible";
+      serviceDivCrr[index].style.opacity = "1";
+      serviceDivCrr[index].style.visibility = "visible";
     });
 
     element.addEventListener("mouseleave", () => {
@@ -21,57 +25,17 @@ if (serviceDiv !== null) {
       serviceTexte[index].style.visibility = "hidden";
       serviceBtn[index].style.opacity = "0";
       serviceBtn[index].style.visibility = "hidden";
+      serviceDivCrr[index].style.opacity = "0";
+      serviceDivCrr[index].style.visibility = "hidden";
     });
   });
 };
-/* ----------------------  Animation Gsap ------------------------------ */
-
-//Detecte si la width du viewport est a 1400px et + pour activer le code Gsap
-let   miniteur;
-if (miniteur !== null) {
-window.addEventListener("resize", (event) => {
-  clearTimeout(miniteur);
-
-  miniteur = setTimeout(restartGsap, 500);
-});
-
-function restartGsap() {
-  if (window.innerWidth > 1399) {
-    scroll_tl = gsap.timeline({
-        scrollTrigger: {
-          trigger: ".crr__titre",
-          start: "top top",
-          scrub: true,
-          end: "bottom bottom",
-          markers: false,
-        },
-      }),
-      roles = document.querySelectorAll(".role");
-
-    scroll_tl.to(roles, {
-      xPercent: -100 * (roles.length - 1),
-      scrollTrigger: {
-        trigger: ".crr__contenue",
-        start: "top top",
-        pin: true,
-        scrub: 1,
-        snap: 2 / (roles.length - 1),
-        end: () => `+=4320`,
-      },
-    });
-  }
-}
-}
-
-
-/* ---------------------- Fin Animation Gsap ------------------------------ */
-
+/* -------------   Anim qui ouvre le menu burger   ------------- */
 function toggleMenu() {
   const menu = document.querySelector(".navbar__menu");
   menu.classList.toggle("active");
-  console.log("ca marche");
 }
-
+/* -------------   Gsap qui rotate le menu burger  ------------- */
 const burgerButton = document.querySelector('.navbar-toggler');
 let isRotated = false;
 
