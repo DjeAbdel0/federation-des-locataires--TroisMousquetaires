198d3e30f512de23e8a67411e285f1690dba2cd9
tc-yavuz <63426295+tc-yavuz@users.noreply.github.com>
2024-12-02 18:14:14 -0500

    about et 404

--- wp-content/themes/flhlmq/partials/404.php
+++ wp-content/themes/flhlmq/partials/404.php
@@ -1,4 +1,4 @@
-<section>
-  <h2>404</h2>
-	<img src="https://media.giphy.com/media/3HLMNi9U0yb4CDldVO/giphy.gif" alt="404">
-</section>
+<div class="erreur">
+  <img class="erreur__404" src="...../themes/page_statique/assets/medias/erreur/maison.jpg">
+  <img class="erreur__img" src="../assets/medias/erreur/maison.jpg">
+</div>
