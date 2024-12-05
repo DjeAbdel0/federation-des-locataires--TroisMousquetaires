e05fe982f7384e12b6b0fef5a067920ff36d5b4d
tc-yavuz <63426295+tc-yavuz@users.noreply.github.com>
2024-12-05 13:30:10 -0500

    modification team et about

--- wp-content/themes/flhlmq/team.php
+++ wp-content/themes/flhlmq/team.php
@@ -1,31 +1,47 @@
 <?php 
 /**
  * 	Template Name: team
- * 	Identique à page, mais avec une barre latérale
+ * 	Template Post Type: page
+
  */
 
 get_header(); // Affiche header.php
+?>
+<div class="equipe">
+      <h2 class="equipe__titre">Équipe de la FLHLMQ</h2>
+	  <?php
 
 if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ? 
-	// Si oui, bouclons au travers les pages (logiquement, il n'y en aura qu'une)
-	while ( have_posts() ) : the_post(); 
+	$teams = new WP_Query('post_type=team');
+ 
+    while ($teams->have_posts()) : $teams->the_post();
 ?>
 
-	<article>
-		<?php if (!is_front_page()) : // Si nous ne sommes PAS sur la page d'accueil ?>
-			<h2>
-				<?php the_title(); // Titre de la page ?>
-			</h2>
-		<?php endif; ?>
-		
-		<?php the_content(); // Contenu principal de la page ?>
-	</article>
+      <!-- Les membres d'equipe -->
+      <div class="equipe__carte">
+        <!-- Carte 1 -->
+        <div class="equipe__membre" data-modal="modal1">
+			<?php the_post_thumbnail('full', ['class' => 'equipe__image']); ?>
+          <h2 class="equipe__nom"><?php the_title(); ?></h2>
+        </div>
+      </div>
+    </div>
+
+
+    <div id="modal1" class="modal">
+      <div class="modal__content">
+        <span class="close">&times;</span>
+        <div class="equipe__info">
+          <p class="equipe__text">
+		  <?php the_content(); ?></p>
+        </div>
+      </div>
+    </div>
 <?php endwhile; // Fermeture de la boucle
 
 else : // Si aucune page n'a été trouvée
 	get_template_part( 'partials/404' ); // Affiche partials/404.php
 endif;
 
-get_sidebar(); // Affiche le contenu de sidebar.php
 get_footer(); // Affiche footer.php 
 ?>
