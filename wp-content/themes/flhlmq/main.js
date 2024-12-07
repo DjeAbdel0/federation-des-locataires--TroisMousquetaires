15d3e8b7ed55574abb0356d0b35627abf0971dcd
Abdel <112128079+DjeAbdel0@users.noreply.github.com>
2024-12-07 17:59:40 -0500

    Tout MAAAAAARCHE!!!!!!!!!!!!!!!!!!

--- wp-content/themes/flhlmq/main.js
+++ wp-content/themes/flhlmq/main.js
@@ -1,4 +1,57 @@
 /* -------------   Abdel    ------------- */
+/* ----------------------  Hover un service, texte visible/Non-visible ------------------------------ */
+const serviceDiv = document.querySelectorAll(".page-service > div"); // Choisis la div enfant directe de .list-service
+const serviceTexte = document.querySelectorAll(".page-service__texte"); // Choisis le texte dans .list-service
+const serviceBtn = document.querySelectorAll(".page-service__btn"); //  Choisis le btn dans .list-service
+const serviceDivCrr = document.querySelectorAll(".page-service-crr > div"); // Choisis la div enfant directe de .list-service
+const serviceTexteCrr = document.querySelectorAll(
+  ".page-service-crr__texte-crr"
+); // Choisis le texte dans .list-service
+
+// Toogle la visibilité selon si la souris Hover ou pas
+if (serviceDiv !== null) {
+  //Met les elements visible
+  serviceDiv.forEach((element, index) => {
+    element.addEventListener("mouseenter", () => {
+      serviceTexte[index].style.opacity = "1";
+      serviceTexte[index].style.visibility = "visible";
+      serviceBtn[index].style.opacity = "1";
+      serviceBtn[index].style.visibility = "visible";
+      serviceDivCrr[index].style.opacity = "1";
+      serviceDivCrr[index].style.visibility = "visible";
+    });
+    //Met les elements caches
+    element.addEventListener("mouseleave", () => {
+      serviceTexte[index].style.opacity = "0";
+      serviceTexte[index].style.visibility = "hidden";
+      serviceBtn[index].style.opacity = "0";
+      serviceBtn[index].style.visibility = "hidden";
+      serviceDivCrr[index].style.opacity = "0";
+      serviceDivCrr[index].style.visibility = "hidden";
+    });
+  });
+}
+/* -------------   Anim qui ouvre le menu burger   ------------- */
+function toggleMenu() {
+  const menu = document.querySelector(".navbar__menu");
+  menu.classList.toggle("active"); //Lui donne la classe ("active")
+}
+/* -------------   Gsap qui rotate le menu burger  ------------- */
+const burgerButton = document.querySelector(".navbar-toggler");
+let isRotated = false;
+
+// Au click du burger
+burgerButton.addEventListener("click", () => {
+  // Utilise GSAP pour animer la rotation du bouton burger
+  gsap.to(burgerButton, {
+    rotation: isRotated ? 0 : 90, // Si le bouton est déjà tourné, on le remet à 0° (sinon, on le fait tourner à 90°)
+    duration: 0.5, // Durée de l'animation en secondes (0.5s)
+    ease: "power1.inOut", // Type de transition de l'animation, ici "inOut" pour un mouvement fluide
+  });
+
+  // Inverse l'état de rotation (true/false)
+  isRotated = !isRotated;
+});
 /* -------------  Fin Abdel    ------------- */
 
 /* -------------   Vincent    ------------- */
